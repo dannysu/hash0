09c55be4c3239e9c050f518e6ae5d2f8e77cc8ab
Danny Su <contact@dannysu.com>
2014-04-20 11:51:47 -0400

    Put back controller unit tests

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -3,21 +3,37 @@
 /* jasmine specs for controllers go here */
 
 describe('controllers', function(){
-    beforeEach(module('hash0.controllers'));
+    beforeEach(function() {
+        module('hash0.controllers');
+        module('hash0.services');
+    });
 
-    it('should have all expected controllers', inject(function($controller) {
-        /*
-        var setupCtrl = $controller('SetupCtrl');
+    it('should have all expected controllers', inject(function($controller, $rootScope, metadata) {
+        var scope = $rootScope.$new();
+        var dispatcherCtrl = $controller('DispatcherCtrl', {
+            '$scope': scope,
+            'metadata': metadata
+        });
+        expect(dispatcherCtrl).toBeDefined();
+
+        var setupCtrl = $controller('SetupCtrl', {
+            '$scope': scope
+        });
         expect(setupCtrl).toBeDefined();
 
-        var unlockCtrl = $controller('UnlockCtrl');
+        var unlockCtrl = $controller('UnlockCtrl', {
+            '$scope': scope
+        });
         expect(unlockCtrl).toBeDefined();
 
-        var generationCtrl = $controller('GenerationCtrl');
+        var generationCtrl = $controller('GenerationCtrl', {
+            '$scope': scope
+        });
         expect(generationCtrl).toBeDefined();
 
-        var mappingCtrl = $controller('MappingCtrl');
+        var mappingCtrl = $controller('MappingCtrl', {
+            '$scope': scope
+        });
         expect(mappingCtrl).toBeDefined();
-        */
     }));
 });
